e6b82fce1c6e41afb59a830b03672642c16c07c7
Ashim Maskey <ashimmaskey4@gmail.com>
2025-03-03 21:17:48 +0545

    made user model

---
+++ Backend/models/User.js
@@ -0,0 +1,24 @@
+const mongoose=require('mongoose');
+
+const userSchema=mongoose.Schema({
+	username:{
+		type: String,
+		required: true
+	},
+	email:{
+		type: String,
+		required: true,
+		unique: true
+	},
+	password:{
+		type: String,
+		required: true,
+	},
+	photo: {
+		type: String,
+		default: ""
+	}
+});
+
+const User=mongoose.model('user', userSchema);
+module.exports=User;
